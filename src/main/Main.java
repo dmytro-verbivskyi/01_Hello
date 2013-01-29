bc990f61692e24b900044655faba1e025a4b156d
dverbovskiy <dverbovskiy@gmail.com>
2013-01-29 14:53:32 +0200

    first

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dverbovskiy
+ * Date: 29.01.13
+ * Time: 13:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("prive23423 t");
+    }
+}
